26548ac04b639d64f043e0245e4b4d6ee2a9528e
Kuba Kazmierczak <kbk.kazmierczak@gmail.com>
2023-11-12 15:15:07 +0000

    Mern project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="grid grid-cols-2 text-center h-screen">
+      <h1 className=" bg-red-300 flex justify-center items-center">
+        Hello world!
+      </h1>
+      <h1 className="bg-blue-500 flex justify-center items-center">Hehe</h1>
+    </div>
+  );
+}
+
+export default App;
